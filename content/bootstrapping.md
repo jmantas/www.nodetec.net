342aa4c894778ecf6d3eefd3fe6addfeca406897
jmantas <jmantas@users.noreply.github.com>
2015-12-10 17:16:09 +0200

    Initial github commit

---
+++ content/bootstrapping.md
@@ -0,0 +1,4 @@
+Title: Bootstrapping...
+Date: 2015-12-05 17:30
+
+This is a place about emerging new and future technologies, about how it will shape our society and how we can become truly network aware civilization. P2P society. Network, not concentrated in big centralized hubs, but spread and distributed around the globe to autonomous entities - nodes.
---
+++ pelicanconf.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'nodetec.net'
+SITENAME = u'NodeTec.Net'
+SITEURL = 'http://www.nodetec.net'
+
+DEFAULT_DATE = 'fs'
+
+SITESUBTITLE = 'Civilization upgrade kit'
+METADATA = 'p2p, futurology, distributed, blockchain, decentralized, trustless, permissionless, third industrial revolution, post scarcity, artificial inteligence'
+PATH = 'content'
+
+TIMEZONE = 'Europe/Vilnius'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+#LINKS = (('Pelican', 'http://getpelican.com/'),
+#         ('Python.org', 'http://python.org/'),
+#         ('Jinja2', 'http://jinja.pocoo.org/'),
+#         ('You can modify those links in your config file', '#'),)
+#
+
+
+# LINKS = (('Pelican', 'http://getpelican.com/'), ('Python.org', 'http://python.org/'))
+
+# Social widget
+#SOCIAL = (('You can add links in your config file', '#'),
+#          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+THEME = "storm"
+# SITESUBTITLE = 'Description of the site'
+# METADATA = 'Meta description is the home page'
+#Pages settings:
+
+#    #Pages
+# DIRECT_TEMPLATES = ('about', 'archives', '404', 'index')
+# DISPLAY_PAGES_ON_MENU  = True
+# ARCHIVES_URL = 'archives/'
+#  ARCHIVES_SAVE_AS = 'archives/index.html'
+#  ABOUT_URL = 'about/'
+#    ABOUT_SAVE_AS = 'about/index.html'
+#  MENUITEMS = (('Archive', 'archives/'),
+#                ('About', 'about/'),)
